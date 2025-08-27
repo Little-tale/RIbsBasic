e418669cab4cdeb110e207d264e2497b7f8c3a63
Kim JaeHyung <you3192001@icloud.com>
2025-08-27 20:44:12 +0900

    [Feat] ViewControllable + 계획 변경 및 추가 설명

---
+++ RibsBasic/Domain/UI/ViewControllable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllable.swift
+//  RibsBasic
+//
+//  Created by Jae hyung Kim on 8/27/25.
+//
+
+import UIKit
+
+public protocol ViewControllable: AnyObject {
+
+    var uiviewController: UIViewController { get }
+}
---
+++ RibsBasic/Utils/UI/ViewControllable.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllable.swift
+//  RibsBasic
+//
+//  Created by Jae hyung Kim on 8/27/25.
+//
+
+import UIKit
+
+extension ViewControllable where Self: UIViewController {
+    
+    var uiviewController: UIViewController {
+        return self
+    }
+}
